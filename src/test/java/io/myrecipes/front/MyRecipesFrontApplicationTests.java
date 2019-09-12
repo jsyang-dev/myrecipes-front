c99a1fa1b6bd53acf1a579c25f98cf429b7cf58e
Jeongsu Yang <mycat83@gmail.com>
2019-09-12 23:03:12 +0900

    기능: 프로젝트 초기 설정
    - front 관련된 dependency 추가
    - tomcat 포트 설정과 thymeleaf 캐싱 해제를 위해서 application.yml 수정
    - 서비스 상태 확인을 위한 HealthController와 template html 생성
    
    해결: #1

--- src/test/java/io/myrecipes/front/MyRecipesFrontApplicationTests.java
+++ src/test/java/io/myrecipes/front/MyRecipesFrontApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MyrecipesFrontApplicationTests {
+public class MyRecipesFrontApplicationTests {
 
     @Test
     public void contextLoads() {
