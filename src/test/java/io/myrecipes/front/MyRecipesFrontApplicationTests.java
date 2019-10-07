b5a9147388a06386c035bc539420093563d78d66
Jeongsu Yang <mycat83@gmail.com>
2019-10-07 23:23:37 +0900

    기능: AWS 설정 테스트중
    * AWSConfiguration 클래스 생성
    * pom.xml 의존성 변경
    
    관련: #12

--- src/test/java/io/myrecipes/front/MyRecipesFrontApplicationTests.java
+++ src/test/java/io/myrecipes/front/MyRecipesFrontApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
@@ -21,6 +22,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
         },
         classes = {MyRecipesFrontApplication.class}
 )
+@TestPropertySource(locations = {"classpath:application.yml", "classpath:aws.yml"})
 public class MyRecipesFrontApplicationTests {
     @Autowired
     DefaultListableBeanFactory beanFactory;
