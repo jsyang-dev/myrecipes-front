e65d7266b58a12861b4be293811542feaa27740c
Jeongsu Yang <mycat83@gmail.com>
2019-10-07 18:29:44 +0900

    기능: 레시피 등록 페이지 개발
    * AmazonS3Client 의존성 주입 테스트
    * S3Config 클래스 생성
    * aws.yml 설정 추가 및 application 시작 시 로드 추가
    * 테스트 작성중
    
    관련: #12

--- src/main/java/io/myrecipes/front/MyRecipesFrontApplication.java
+++ src/main/java/io/myrecipes/front/MyRecipesFrontApplication.java
@@ -1,13 +1,18 @@
 package io.myrecipes.front;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class MyRecipesFrontApplication {
+    private static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "classpath:aws.yml";
 
     public static void main(String[] args) {
-        SpringApplication.run(MyRecipesFrontApplication.class, args);
+        new SpringApplicationBuilder(MyRecipesFrontApplication.class)
+                .profiles(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
 }
