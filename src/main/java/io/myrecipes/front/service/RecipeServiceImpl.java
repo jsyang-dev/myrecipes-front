a2cf43b43106d062ea9c3f37d81a852340fe4af9
Jeongsu Yang <mycat83@gmail.com>
2019-10-10 23:41:24 +0900

    기능: 레시피 등록 페이지 개발
    * 이미지 업로드 기능 구현
    
    관련: #12

--- src/main/java/io/myrecipes/front/service/RecipeServiceImpl.java
+++ src/main/java/io/myrecipes/front/service/RecipeServiceImpl.java
@@ -1,14 +1,17 @@
 package io.myrecipes.front.service;
 
 import io.myrecipes.front.common.RestTemplateHelperImpl;
+import io.myrecipes.front.common.S3HelperImpl;
 import io.myrecipes.front.dto.Material;
 import io.myrecipes.front.dto.Recipe;
 import io.myrecipes.front.dto.RecipeRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -23,9 +26,11 @@ public class RecipeServiceImpl implements RecipeService {
     private String port;
 
     private final RestTemplateHelperImpl restTemplateHelper;
+    private final S3HelperImpl s3Helper;
 
-    public RecipeServiceImpl(RestTemplateHelperImpl restTemplateHelper) {
+    public RecipeServiceImpl(RestTemplateHelperImpl restTemplateHelper, S3HelperImpl s3Helper) {
         this.restTemplateHelper = restTemplateHelper;
+        this.s3Helper = s3Helper;
     }
 
     @Override
@@ -50,5 +55,18 @@ public class RecipeServiceImpl implements RecipeService {
                 .build(true);
 
         return this.restTemplateHelper.postForEntity(Recipe.class, uriComponents.toUriString(), recipeRequest);
+    }
+
+    @Override
+    public String uploadImage(MultipartFile file, String path) {
+        String imageUrl = "";
+
+        try {
+            imageUrl = this.s3Helper.upload(file, path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return imageUrl;
     }
 }
