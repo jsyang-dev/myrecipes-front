ce5bc6c8ebcf8210e3601a6d2d45eae18021e79e
Jeongsu Yang <mycat83@gmail.com>
2019-10-13 23:22:13 +0900

    기능: 레시피 등록 페이지 개발
    * dto에 validation 애노테이션 적용
    * 레시피 등록 템플릿에 validation 메시지 표시기능 추가
    
    관련: #12

--- src/main/java/io/myrecipes/front/dto/RecipeMaterial.java
+++ src/main/java/io/myrecipes/front/dto/RecipeMaterial.java
@@ -4,14 +4,16 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 @Getter
 @NoArgsConstructor
 public class RecipeMaterial {
     private Integer materialId;
 
-//    @NotBlank(message = "요리재료의 수량을 선택해주세요.")
+    @NotNull(message = "요리 재료의 수량을 선택해주세요.")
+    @Positive(message = "요리 재료의 수량을 양수로 입력해주세요.")
     private Integer quantity;
 
     @Builder
--- src/main/java/io/myrecipes/front/dto/RecipeRequest.java
+++ src/main/java/io/myrecipes/front/dto/RecipeRequest.java
@@ -3,35 +3,40 @@ package io.myrecipes.front.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @NoArgsConstructor
 public class RecipeRequest {
-//    @NotBlank(message = "레시피 제목을 입력해주세요.")
+    @NotBlank(message = "레시피 제목을 입력해주세요.")
+    @Length(max = 60, message = "레시피 제목은 최대 60자까지 입력이 가능합니다.")
     private String title;
 
-//    @NotBlank(message = "레시피 이미지를 등록해주세요.")
+    @NotBlank(message = "레시피 이미지를 등록해주세요.")
+    @Length(max = 200, message = "레시피 이미지는 최대 200자까지 입력이 가능합니다.")
     private String image;
 
-//    @NotBlank(message = "예상시간을 선택해주세요.")
+    @NotNull(message = "예상시간을 선택해주세요.")
+    @Positive(message = "양수로 입력해주세요.")
     private Integer estimatedTime;
 
-//    @NotBlank(message = "난이도를 선택해주세요.")
-//    @Min(1)
-//    @Max(5)
+    @NotNull(message = "난이도를 선택해주세요.")
+    @Min(value = 1, message = "난이도는 최소 1부터 입력이 가능합니다.")
+    @Max(value = 5, message = "난이도는 최대 5까지 입력이 가능합니다.")
     private Integer difficulty;
 
-    private List<RecipeMaterial> recipeMaterialList = new ArrayList<>();
+    @NotEmpty(message = "요리 재료를 1개 이상 입력해주세요.")
+    private List<@Valid RecipeMaterial> recipeMaterialList = new ArrayList<>();
 
-    private List<RecipeStep> recipeStepList = new ArrayList<>();
+    @NotEmpty(message = "요리 순서를 1개 이상 입력해주세요.")
+    private List<@Valid RecipeStep> recipeStepList = new ArrayList<>();
 
-    private List<RecipeTag> recipeTagList = new ArrayList<>();
+    private List<@Valid RecipeTag> recipeTagList = new ArrayList<>();
 
     @Builder
     public RecipeRequest(String title, String image, Integer estimatedTime, Integer difficulty) {
