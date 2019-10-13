a33cfc1d29fb87ea970bf91adb8d6a08b9547063
Jeongsu Yang <mycat83@gmail.com>
2019-10-13 23:22:13 +0900

    기능: 레시피 등록 페이지 개발
    * dto에 validation 애노테이션 적용
    * 레시피 등록 템플릿에 validation 메시지 표시기능 추가
    
    관련: #12

--- src/main/java/io/myrecipes/front/dto/RecipeStep.java
+++ src/main/java/io/myrecipes/front/dto/RecipeStep.java
@@ -3,6 +3,7 @@ package io.myrecipes.front.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 
@@ -11,9 +12,10 @@ import javax.validation.constraints.NotBlank;
 public class RecipeStep {
     private Integer step;
 
-//    @NotBlank(message = "요리 순서의 내용을 입력해주세요.")
+    @NotBlank(message = "요리 순서의 내용을 입력해주세요.")
     private String content;
 
+    @Length(max = 200, message = "요리 순서는 최대 200자까지 입력이 가능합니다.")
     private String image;
 
     @Builder
