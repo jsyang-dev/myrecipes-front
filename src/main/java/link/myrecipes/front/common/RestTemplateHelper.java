afe6d1a99a1947cbe3b95ed9573efabe51292a50
Jeongsu Yang <mycat83@gmail.com>
2019-11-02 22:22:53 +0900

    기능: RestTemplateHelper 기능 추가
    * putForEntity 메소드 추가

--- src/main/java/link/myrecipes/front/common/RestTemplateHelper.java
+++ src/main/java/link/myrecipes/front/common/RestTemplateHelper.java
@@ -8,4 +8,6 @@ public interface RestTemplateHelper {
     <T> List<T> getForList(Class<T> clazz, String url, Object... uriVariables);
 
     <T, R> T postForEntity(Class<T> clazz, String url, R body, Object... uriVariables);
+
+    <T, R> T putForEntity(Class<T> clazz, String url, R body, Object... uriVariables);
 }
--- src/main/java/link/myrecipes/front/common/RestTemplateHelperImpl.java
+++ src/main/java/link/myrecipes/front/common/RestTemplateHelperImpl.java
@@ -6,12 +6,14 @@ import com.fasterxml.jackson.databind.type.CollectionType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 @Component
 @Slf4j
@@ -25,14 +27,17 @@ public class RestTemplateHelperImpl implements RestTemplateHelper {
     }
 
     @Override
-    // TODO: 반환값이 에러메시지인 경우에 대한 처리 ApiErrorInfo에 대한 매핑 처리
     public <T> T getForEntity(Class<T> clazz, String url, Object... uriVariables) {
         ResponseEntity<String> response = this.restTemplate.getForEntity(url, String.class, uriVariables);
-        JavaType type =this. objectMapper.getTypeFactory().constructType(clazz);
+        JavaType type = this.objectMapper.getTypeFactory().constructType(clazz);
         T result = null;
 
         try {
-            result = this.objectMapper.readValue(response.getBody(), type);
+            if (clazz == String.class) {
+                result = (T) response.getBody();
+            } else {
+                result = this.objectMapper.readValue(response.getBody(), type);
+            }
         } catch (IOException e) {
             log.info(e.getMessage());
         }
@@ -47,7 +52,7 @@ public class RestTemplateHelperImpl implements RestTemplateHelper {
         List<T> result = null;
 
         try {
-            result = this.objectMapper.readValue(response.getBody(), type);
+            result = this.objectMapper.readValue(Objects.requireNonNull(response.getBody()), type);
         } catch (IOException e) {
             log.info(e.getMessage());
         }
@@ -58,12 +63,36 @@ public class RestTemplateHelperImpl implements RestTemplateHelper {
     @Override
     public <T, R> T postForEntity(Class<T> clazz, String url, R body, Object... uriVariables) {
         HttpEntity<R> request = new HttpEntity<>(body);
-        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class, uriVariables);
-        JavaType type = objectMapper.getTypeFactory().constructType(clazz);
+        ResponseEntity<String> response = this.restTemplate.postForEntity(url, request, String.class, uriVariables);
+        JavaType type = this.objectMapper.getTypeFactory().constructType(clazz);
         T result = null;
 
         try {
-            result = this.objectMapper.readValue(response.getBody(), type);
+            if (clazz == String.class) {
+                result = (T) response.getBody();
+            } else {
+                result = this.objectMapper.readValue(Objects.requireNonNull(response.getBody()), type);
+            }
+        } catch (IOException e) {
+            log.info(e.getMessage());
+        }
+
+        return result;
+    }
+
+    @Override
+    public <T, R> T putForEntity(Class<T> clazz, String url, R body, Object... uriVariables) {
+        HttpEntity<R> request = new HttpEntity<>(body);
+        ResponseEntity<String> response = this.restTemplate.exchange(url, HttpMethod.PUT, request, String.class, uriVariables);
+        JavaType type = this.objectMapper.getTypeFactory().constructType(clazz);
+        T result = null;
+
+        try {
+            if (clazz == String.class) {
+                result = (T) response.getBody();
+            } else {
+                result = this.objectMapper.readValue(Objects.requireNonNull(response.getBody()), type);
+            }
         } catch (IOException e) {
             log.info(e.getMessage());
         }
