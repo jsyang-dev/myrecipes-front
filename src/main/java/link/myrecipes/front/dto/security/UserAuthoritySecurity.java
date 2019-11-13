cf907a83cb31accad8cd51fc199c55c0f0795c07
Jeongsu Yang <mycat83@gmail.com>
2019-11-13 23:21:35 +0900

    기능: 로그인 구현
    * pom.xml: 스프링 시큐리티 의존성 추가
    * 로그인 관련 dto 생성
    
    관련: #27

---
+++ src/main/java/link/myrecipes/front/dto/security/UserAuthoritySecurity.java
@@ -0,0 +1,13 @@
+package link.myrecipes.front.dto.security;
+
+public class UserAuthoritySecurity {
+    private String authority;
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
---
+++ src/main/java/link/myrecipes/front/dto/security/UserSecurity.java
@@ -0,0 +1,59 @@
+package link.myrecipes.front.dto.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class UserSecurity implements UserDetails {
+    private String username;
+
+    private String password;
+
+    private boolean accountNonExpired;
+
+    private boolean accountNonLocked;
+
+    private boolean credentialsNonExpired;
+
+    private boolean enabled;
+
+    private List<UserAuthoritySecurity> userAuthoritySecurityList = new ArrayList<>();
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+}
