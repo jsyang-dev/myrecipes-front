8bc1c79ecf31e22ac8c33a993d8a245bf7a1c33d
Jeongsu Yang <mycat83@gmail.com>
2019-11-18 22:15:24 +0900

    기능: MemberControllerTest 구현
    * MockMvc 기반 Controller 테스트 구현

--- src/test/java/link/myrecipes/front/controller/MemberControllerTest.java
+++ src/test/java/link/myrecipes/front/controller/MemberControllerTest.java
@@ -3,18 +3,22 @@ package link.myrecipes.front.controller;
 import link.myrecipes.front.dto.User;
 import link.myrecipes.front.dto.request.UserRequest;
 import link.myrecipes.front.service.MemberServiceImpl;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -29,14 +33,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 )
 @AutoConfigureMockMvc
 public class MemberControllerTest {
+    private User user;
+
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private MemberServiceImpl memberService;
 
+    @Before
+    public void setUp() {
+        this.user = User.builder()
+                .id(1)
+                .username("user12")
+                .password("123456")
+                .name("유저12")
+                .phone("01012345678")
+                .email("user12@domain.com")
+                .build();
+    }
+
     @Test
-    public void Should_정상_리턴_When_회원가입_페이지_호출() throws Exception {
+    public void Should_정상_리턴_When_회원가입_페이지_조회() throws Exception {
         //when
         final ResultActions actions = this.mockMvc.perform(get("/member/register"));
 
@@ -49,17 +67,9 @@ public class MemberControllerTest {
     }
 
     @Test
-    public void Should_리다이렉트_When_회원가입_POST_호출() throws Exception {
+    public void Should_리다이렉트_When_회원가입_저장() throws Exception {
         //given
-        User user = User.builder()
-                .id(1)
-                .username("user12")
-                .password("123456")
-                .name("유저12")
-                .phone("01012345678")
-                .email("user12@domain.com")
-                .build();
-        given(this.memberService.createMember(user.toRequestDTO())).willReturn(user);
+        given(this.memberService.createMember(any(UserRequest.class))).willReturn(this.user);
 
         //when
         final ResultActions actions = this.mockMvc.perform(post("/member/register")
@@ -74,5 +84,79 @@ public class MemberControllerTest {
         actions.andDo(print())
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    public void Should_리다이렉트_When_회원가입_파라미터없이_저장() throws Exception {
+        //when
+        final ResultActions actions = this.mockMvc.perform(post("/member/register")
+                .with(csrf()));
+
+        //then
+        actions.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("member/register"))
+                .andExpect(model().hasErrors())
+                .andExpect(content().string(containsString("_csrf")))
+                .andExpect(content().string(containsString("error-message")));
+    }
+
+    @Test
+    @WithMockUser
+    public void Should_정상_리턴_When_회원정보수정_페이지_조회() throws Exception {
+        //given
+        given(this.memberService.readMember()).willReturn(this.user);
+
+        //when
+        final ResultActions actions = this.mockMvc.perform(get("/member/modify"));
+
+        //then
+        actions.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("member/modify"))
+                .andExpect(model().attribute("userRequest", instanceOf(UserRequest.class)))
+                .andExpect(model().attribute("userId", this.user.getId()))
+                .andExpect(content().string(containsString("_csrf")))
+                .andExpect(content().string(containsString("value=\"" + user.getUsername() + "\"")))
+                .andExpect(content().string(containsString("value=\"" + user.getName() + "\"")))
+                .andExpect(content().string(containsString("value=\"" + user.getPhone() + "\"")))
+                .andExpect(content().string(containsString("value=\"" + user.getEmail() + "\"")));
+    }
+
+    @Test
+    @WithMockUser
+    public void Should_리다이렉트_When_회원정보수정_저장() throws Exception {
+        //given
+        given(this.memberService.updateMember(eq(this.user.getId()), any(UserRequest.class))).willReturn(this.user);
+
+        //when
+        final ResultActions actions = this.mockMvc.perform(post("/member/modify/" + this.user.getId())
+                .param("username", user.getUsername())
+                .param("password", user.getPassword())
+                .param("name", user.getName())
+                .param("phone", user.getPhone())
+                .param("email", user.getEmail())
+                .with(csrf()));
+
+        //then
+        actions.andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    @WithMockUser
+    public void Should_리다이렉트_When_회원정보수정_파라미터없이_저장() throws Exception {
+        //when
+        final ResultActions actions = this.mockMvc.perform(post("/member/modify/" + this.user.getId())
+                .with(csrf()));
+
+        //then
+        actions.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("member/modify"))
+                .andExpect(model().hasErrors())
+                .andExpect(content().string(containsString("_csrf")))
+                .andExpect(content().string(containsString("error-message")));
     }
 }
