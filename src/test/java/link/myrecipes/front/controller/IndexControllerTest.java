61f74dc949ca89789ce8759b1d4af36b9c89d091
Jeongsu Yang <mycat83@gmail.com>
2020-02-21 22:01:48 +0900

    기능: 테스트 개선
    - IndexControllerTest 수정
    
    해결: #51

--- src/test/java/link/myrecipes/front/controller/IndexControllerTest.java
+++ src/test/java/link/myrecipes/front/controller/IndexControllerTest.java
@@ -1,12 +1,22 @@
 package link.myrecipes.front.controller;
 
-import link.myrecipes.front.dto.PageParam;
 import link.myrecipes.front.dto.Recipe;
 import link.myrecipes.front.service.IndexService;
 import org.junit.Test;
 import org.mockito.ArgumentMatcher;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.Collections;
+
+import static org.hamcrest.Matchers.contains;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 public class IndexControllerTest extends ControllerTest {
@@ -28,40 +38,47 @@ public class IndexControllerTest extends ControllerTest {
 
     @Test
     public void 메인_페이지_호출() throws Exception {
-        //given
-        Recipe recipe = Recipe.builder().title("test1").image("image1.jpg").estimatedTime(30).difficulty(1).build();
-        PageParam pageParam = PageParam.builder().page(1).size(this.pageSize).sortField(this.sortField).isDescending(this.isDescending).build();
 
-//        given(indexService.readPopularRecipeList()).willReturn(Collections.singletonList(recipe));
-//        given(this.indexService.readRecipeList(argThat(new PageParamMatcher(pageParam)))).willReturn(Collections.singletonList(recipe));
-//        given(this.indexService.readRecipePageCount()).willReturn(1);
-//
-//        //when
-//        final ResultActions actions = this.mockMvc.perform(get("/index"));
-//
-//        //then
-//        actions.andExpect(status().isOk())
-//                .andExpect(view().name("index"))
-//                .andExpect(model().attributeExists("popularRecipeList", "newRecipeList", "recipePageCount", "recipeImagePath"))
-//                .andExpect(model().attribute("popularRecipeList", contains(recipe)))
-//                .andExpect(model().attribute("newRecipeList", contains(recipe)))
-//                .andExpect(model().attribute("recipePageCount", 1))
-//                .andExpect(model().attribute("recipeImagePath", this.recipeImagePath));
+        // Given
+        Sort.Direction direction;
+        if (isDescending) {
+            direction = Sort.Direction.DESC;
+        } else {
+            direction = Sort.Direction.ASC;
+        }
+        PageRequest pageRequest = PageRequest.of(0, this.pageSize, direction,this.sortField);
+        Recipe recipe = Recipe.builder().title("test1").image("image1.jpg").estimatedTime(30).difficulty(1).build();
+
+        given(this.indexService.readPopularRecipeList()).willReturn(Collections.singletonList(recipe));
+        given(this.indexService.readRecipeList(argThat(new PageRequestMatcher(pageRequest)))).willReturn(Collections.singletonList(recipe));
+        given(this.indexService.readRecipePageCount()).willReturn(1);
+
+        // When
+        final ResultActions actions = this.mockMvc.perform(get("/index"));
+
+        // Then
+        actions.andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(model().attributeExists("popularRecipeList", "newRecipeList", "recipePageCount", "recipeImagePath"))
+                .andExpect(model().attribute("popularRecipeList", contains(recipe)))
+                .andExpect(model().attribute("newRecipeList", contains(recipe)))
+                .andExpect(model().attribute("recipePageCount", 1))
+                .andExpect(model().attribute("recipeImagePath", this.recipeImagePath));
     }
 
-    static class PageParamMatcher implements ArgumentMatcher<PageParam> {
-        private PageParam left;
+    static class PageRequestMatcher implements ArgumentMatcher<PageRequest> {
 
-        PageParamMatcher(PageParam left) {
+        private PageRequest left;
+
+        PageRequestMatcher(PageRequest left) {
             this.left = left;
         }
 
         @Override
-        public boolean matches(PageParam right) {
-            return left.getPage() == right.getPage()
-                    && left.getSize() == right.getSize()
-                    && left.getSortField().equals(right.getSortField())
-                    && left.isDescending() == right.isDescending();
+        public boolean matches(PageRequest right) {
+            return left.getPageNumber() == right.getPageNumber()
+                    && left.getPageSize() == right.getPageSize()
+                    && left.getSort().equals(right.getSort());
         }
     }
 }
